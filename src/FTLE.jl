156570ee0be1b36e9933e631ae3fc93e0e86e604
qiyuanbillwu <billwu2003@gmail.com>
2024-09-17 23:42:01 -0700

    euler_backward

--- src/FTLE.jl
+++ src/FTLE.jl
@@ -18,3 +18,25 @@ function euler_forward(initial_conditions, u, v, t0, t_start, dt, T)
     return w
 
 end
+
+function euler_backward(initial_conditions, u, v, t0, t_start, dt, T)
+    # this is not backward euler, it is forward euler method going back in time
+    # t0 is the initial time 
+    # t_start is where the starting time of the solution from viscous flow
+    # dt is the interval between consecutive u and v fields
+    # T is the integration time
+
+    start_idx = Int(round((t0 - t_start) / dt + 1))
+    iters = Int(round(T / dt - 1))
+
+    z = initial_conditions
+
+    for i = start_idx:-1:start_idx - iters
+        x = z[:, 1]
+        y = z[:, 2]
+        z = z - dt * [u[i].(x, y) v[i].(x, y)]
+    end
+
+    return z
+
+end
