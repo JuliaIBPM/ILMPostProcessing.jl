4ecf2f7b88debf389e490807ede19d77482173bd
qiyuanbillwu <billwu2003@gmail.com>
2024-09-17 23:42:47 -0700

    compute FTLE function

--- src/FTLE.jl
+++ src/FTLE.jl
@@ -40,3 +40,37 @@ function euler_backward(initial_conditions, u, v, t0, t_start, dt, T)
     return z
 
 end
+
+function compute_FTLE(FTLE, nx, ny, T, final_x, final_y)
+    
+    # Shifted arrays for vector operations
+    final_x_i_minus = final_x[2:end-1, 1:end-2]
+    final_x_i_plus  = final_x[2:end-1, 3:end]
+    final_x_j_minus = final_x[1:end-2, 2:end-1]
+    final_x_j_plus  = final_x[3:end, 2:end-1]
+
+    final_y_i_minus = final_y[2:end-1, 1:end-2]
+    final_y_i_plus  = final_y[2:end-1, 3:end]
+    final_y_j_minus = final_y[1:end-2, 2:end-1]
+    final_y_j_plus  = final_y[3:end, 2:end-1]
+
+    # Compute the elements of the deformation gradient tensor A
+    a11 = (final_x_i_plus - final_x_i_minus) / 2 / dx
+    a12 = (final_x_j_plus - final_x_j_minus) / 2 / dy
+    a21 = (final_y_i_plus - final_y_i_minus) / 2 / dx
+    a22 = (final_y_j_plus - final_y_j_minus) / 2 / dy
+
+    # Compute delta components
+    delta_11 = a11.^2 .+ a21.^2
+    delta_12 = a11 .* a12 .+ a21 .* a22
+    delta_22 = a12.^2 .+ a22.^2
+
+    # Eigenvalues of the delta matrix using characteristic equation
+    trace = delta_11 .+ delta_22
+    determinant = delta_11 .* delta_22 .- delta_12.^2
+    discriminant = sqrt.(trace.^2 .- 4 .* determinant)
+    lambda1 = (trace .+ discriminant) ./ 2
+
+    # Compute FTLE (same slicing approach to match the dimensions)
+    FTLE .= 1 / (2 * abs(T)) .* log.(lambda1)
+end
