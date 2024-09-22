ab404d917ce6520f371ebaed7125652b263576c1
qiyuanbillwu <billwu2003@gmail.com>
2024-09-22 14:46:47 -0700

    testing computing FTLE on empty viscous flow problem

--- test/FTLE.jl
+++ test/FTLE.jl
@@ -1,5 +1,72 @@
+using ILMPostProcessing
+using ViscousFlow
+using LinearAlgebra
+
 @testset "FTLE" begin
 
-    @test 1 + 1 == 2  # Passes
+    # this test setup up an empty problem to compute FTLE fields on zero velocity fields
+    # setup and solve viscous flow problem
+    my_params = Dict()
+    my_params["Re"] = 200
+    xlim = (-1.0,1.0)
+    ylim = (-1.0,1.0)
+
+    g = setup_grid(xlim,ylim,my_params)
+    sys = viscousflow_system(g,phys_params=my_params)
+
+    u0 = init_sol(sys)
+
+    tspan = (0.0,1.0)
+    integrator = init(u0,tspan,sys)
+    step!(integrator,1.0)
+    sol = integrator.sol
+
+    # generate velocity fields
+    u = []
+    v = []
+    t_start = 0.0
+    t_end = 1.0
+    dt = 0.1
+
+    ILMPostProcessing.make_interp_fields!(u, v, t_start, t_end, dt, ViscousFlow.velocity, sol, sys, g)
+
+    @test norm(u[end]) < 1e-8
+    @test norm(v[end]) < 1e-8
+
+    # generate initial conditions
+    X_MIN = -0.5
+    X_MAX = 0.5
+    Y_MIN = -0.5
+    Y_MAX = 0.5
+    nx, ny = 10, 10
+
+    initial_conditions, dx, dy = ILMPostProcessing.gen_init_conds(X_MIN, X_MAX, Y_MIN, Y_MAX, nx, ny)
+
+    # solve the forward and backward IVP using Euler and Adams-Bashforth
+    # start at t = 0.5 and integrate to t= 1.0 in forward time and to t = 0.0 in backward time
+    T = 0.5
+    t0 = 0.5
+
+    a = ILMPostProcessing.euler_forward(initial_conditions, u, v, t0, t_start, dt, T)
+    b = ILMPostProcessing.euler_backward(initial_conditions, u, v, t0, t_start, dt, T)
+    c = ILMPostProcessing.adams_bashforth_2_forward(initial_conditions, u, v, t0, t_start, dt, T)
+    d = ILMPostProcessing.adams_bashforth_2_backward(initial_conditions, u, v, t0, t_start, dt, T)
+
+    @test norm(a - initial_conditions) < 1e-8
+    @test norm(b - initial_conditions) < 1e-8
+    @test norm(c - initial_conditions) < 1e-8
+    @test norm(d - initial_conditions) < 1e-8
+
+    # compute FTLE fields, should all be zeros
+    ftle_a = ftle_b = ftle_c = ftle_d = zeros(Float64, ny - 2, nx - 2)
+    ILMPostProcessing.compute_FTLE!(ftle_a, nx, ny, T, a, dx, dy)
+    ILMPostProcessing.compute_FTLE!(ftle_b, nx, ny, T, b, dx, dy)
+    ILMPostProcessing.compute_FTLE!(ftle_c, nx, ny, T, c, dx, dy)
+    ILMPostProcessing.compute_FTLE!(ftle_d, nx, ny, T, d, dx, dy)
+
+    @test norm(ftle_a) < 1e-8
+    @test norm(ftle_b) < 1e-8
+    @test norm(ftle_c) < 1e-8
+    @test norm(ftle_d) < 1e-8
 
 end
