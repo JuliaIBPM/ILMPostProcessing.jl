0adcd81909fe67d722db27f33705f9b528560ca7
Jeff Eldredge <jdeldre@g.ucla.edu>
2024-09-23 14:28:37 -0700

    Added initial routines for trajectory calcs

--- src/ILMPostProcessing.jl
+++ src/ILMPostProcessing.jl
@@ -4,9 +4,10 @@ using ImmersedLayers
 using LinearAlgebra
 using Statistics
 
-export pod, dmd, PODModes, DMDModes, compute_FTLE!
+export pod, dmd, PODModes, DMDModes, compute_FTLE!, compute_trajectory
 
 include("utilities.jl")
+include("trajectories.jl")
 include("POD.jl")
 include("DMD.jl")
 include("FTLE.jl")
