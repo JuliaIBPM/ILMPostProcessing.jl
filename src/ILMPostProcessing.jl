9a083a6b9318f669deecda3dc3bbc1e7560a5751
吴其远 <wuqiyuan@s-MacBook-Pro.local>
2024-09-23 19:51:03 -0700

    export FTLE functions

--- src/ILMPostProcessing.jl
+++ src/ILMPostProcessing.jl
@@ -4,7 +4,7 @@ using ImmersedLayers
 using LinearAlgebra
 using Statistics
 
-export pod, dmd, PODModes, DMDModes, compute_FTLE!
+export pod, dmd, PODModes, DMDModes, make_interp_fields!, gen_init_conds, euler_forward, euler_backward, adams_bashforth_2_forward, adams_bashforth_2_backward, compute_FTLE!
 
 include("utilities.jl")
 include("POD.jl")
