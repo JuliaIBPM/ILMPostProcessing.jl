5a743ba7a24673fffd1b589a06571985107b0bc6
Jeff Eldredge <jdeldre@g.ucla.edu>
2024-09-24 12:19:31 -0700

    Added more trajectory routines and a modified FTLE routine

--- src/trajectories.jl
+++ src/trajectories.jl
@@ -1,9 +1,25 @@
 # TRAJECTORY CALCULATION #
 
+import ImmersedLayers.CartesianGrids.Interpolations: AbstractInterpolation
+
 const DEFAULT_DT = 0.001
 const DEFAULT_DT_STREAK = 0.01
+const DEFAULT_ALG = Euler()
 
 ## APIs ##
+
+function compute_trajectory(vr::Vector{Tuple{T,T}},tr::StepRangeLen,X₀,Trange::Tuple;Δt::Real=step(tr),alg=ILMPostProcessing.DEFAULT_ALG,kwargs...) where T<:CartesianGrids.Interpolations.AbstractInterpolation
+  @assert length(vr) == length(tr) "Supplied time array must be same length as supplied velocity array"
+  ti, tf = _check_times(tr,Trange,Δt)
+  _dt = sign(tf-ti)*Δt
+
+  vfcn!(dR,R,p,t) = _vfcn_interpolated_series!(dR,R,p,t,vr,tr)
+
+  sol = ILMPostProcessing._solve_trajectory(vfcn!,X₀,Trange,_dt,alg;kwargs...)
+  return sol
+  
+end
+
 
 
 """
@@ -14,8 +30,8 @@ Calculate the trajectory of a particle with initial location `X₀`. The argumen
 integration time, and `Δt` is the time step size. The output is the solution
 structure for the `OrdinaryDiffEq` package.
 """
-compute_trajectory(u::Edges, sys::ILMSystem, X₀::Union{Vector{S},Vector{Vector{S}}},Trange::Tuple;Δt=DEFAULT_DT) where S <: Real =
-    compute_trajectory(interpolatable_field(u,sys.base_cache.g)...,X₀,Trange;Δt=Δt)
+compute_trajectory(u::Edges, sys::ILMSystem, X₀::Union{Vector{S},Vector{Vector{S}}},Trange::Tuple;Δt=DEFAULT_DT,alg=DEFAULT_ALG,kwargs...) where S <: Real =
+    compute_trajectory(interpolatable_field(u,sys.base_cache.g)...,X₀,Trange;Δt=Δt,alg=alg,kwargs...)
 
 
 
@@ -34,21 +50,21 @@ of such solution structures).
 """
 function compute_trajectory(ufield::AbstractInterpolation{T,2},
                             vfield::AbstractInterpolation{T,2},
-                            X₀::Vector{S},Trange::Tuple;Δt::Real=DEFAULT_DT) where {T,S<:Real}
+                            X₀::Vector{S},Trange::Tuple;Δt::Real=DEFAULT_DT,alg=DEFAULT_ALG,kwargs...) where {T,S<:Real}
 
   vfcn!(dR,R,p,t) = _vfcn_autonomous!(dR,R,p,t,ufield,vfield)
 
-  sol = _solve_trajectory(vfcn!,X₀,Trange,Δt)
+  sol = _solve_trajectory(vfcn!,X₀,Trange,Δt,alg;kwargs...)
   return sol
 
 end
 
 function compute_trajectory(ufield::T,vfield::T,
-   pts::Vector{Vector{S}},Trange::Tuple;Δt=DEFAULT_DT) where {S<:Real,T<:Union{AbstractInterpolation,Function}}
+   pts::Vector{Vector{S}},Trange::Tuple;Δt=DEFAULT_DT,alg=DEFAULT_ALG,kwargs...) where {S<:Real,T<:Union{AbstractInterpolation,Function}}
 
   sol_array = ODESolution[]
   for X₀ in pts
-    sol = compute_trajectory(ufield,vfield,X₀,Trange,Δt=Δt)
+    sol = compute_trajectory(ufield,vfield,X₀,Trange;Δt=Δt,alg=alg,kwargs...)
     push!(sol_array,sol)
   end
   return sol_array
@@ -57,11 +73,11 @@ end
 
 function compute_trajectory(ufcn::Function,
                             vfcn::Function,
-                            X₀::Vector{S},Trange::Tuple;Δt::Real=DEFAULT_DT) where {S<:Real}
+                            X₀::Vector{S},Trange::Tuple;Δt::Real=DEFAULT_DT,alg=DEFAULT_ALG,kwargs...) where {S<:Real}
 
   velfcn(R,p,t) = _vfcn_nonautonomous(R,p,t,ufcn,vfcn)
 
-  sol = _solve_trajectory(velfcn,X₀,Trange,Δt)
+  sol = _solve_trajectory(velfcn,X₀,Trange,Δt,alg;kwargs...)
   return sol
 
 end
@@ -82,21 +98,21 @@ of such solution structures).
 """
 function compute_streamline(ufcn::Function,
                             vfcn::Function,
-                            X₀::Vector{S},srange::Tuple,t::Real;Δs::Real=DEFAULT_DT) where {S<:Real}
+                            X₀::Vector{S},srange::Tuple,t::Real;Δs::Real=DEFAULT_DT,alg=DEFAULT_ALG,kwargs...) where {S<:Real}
 
   velfcn(R,p,s) = _vfcn_nonautonomous_frozentime(R,p,s,ufcn,vfcn)
 
-  sol = _solve_streamline(velfcn,X₀,srange,Δs,t)
+  sol = _solve_streamline(velfcn,X₀,srange,Δs,t,alg;kwargs...)
   return sol
 
 end
 
 function compute_streamline(ufield,vfield,
-   pts::Vector{Vector{S}},srange::Tuple,t::Real;Δs=DEFAULT_DT) where {S<:Real}
+   pts::Vector{Vector{S}},srange::Tuple,t::Real;Δs=DEFAULT_DT,alg=DEFAULT_ALG,kwargs...) where {S<:Real}
 
   sol_array = ODESolution[]
   for X₀ in pts
-    sol = compute_streamline(ufield,vfield,X₀,srange,t,Δs=Δs)
+    sol = compute_streamline(ufield,vfield,X₀,srange,t;Δs=Δs,alg=alg,kwargs...)
     push!(sol_array,sol)
   end
   return sol_array
@@ -113,17 +129,18 @@ units before the current instant `t`. The time step size `Δt` sets the resoluti
 of the streakline (i.e., how often the particles are sampled along the streakline).
 It returns arrays of the x and y coordinates of the streakline.
 """
-function compute_streakline(u,v,X₀::Vector{S},t;τmin = t-3.0, Δtstreak::Real=DEFAULT_DT_STREAK, Δttraj::Real=DEFAULT_DT) where {S<:Real}
+function compute_streakline(u,v,X₀::Vector{S},t;τmin = t-3.0, Δtstreak::Real=DEFAULT_DT_STREAK, Δttraj::Real=DEFAULT_DT,alg=DEFAULT_ALG,kwargs...) where {S<:Real}
   τstreak = τmin:Δtstreak:t
   xstreak = zeros(length(τstreak))
   ystreak = zeros(length(τstreak))
 
   for (i,τ) in enumerate(τstreak)
-    traj = compute_trajectory(u,v,X₀,(τ,t),Δt = Δttraj)
+    traj = compute_trajectory(u,v,X₀,(τ,t);Δt = Δttraj,alg=alg,kwargs...)
     xstreak[i], ystreak[i] = traj.u[end]
   end
   return xstreak, ystreak
 end
+
 
 """
   field_along_trajectory(f::GridData,sys::NavierStokes,traj::ODESolution[,deriv=0])
@@ -143,14 +160,14 @@ field_along_trajectory(d::GridData,sys,traj;deriv=0) = _field_along_trajectory(d
 
 
 
-function _solve_trajectory(vfcn,u0,Trange,Δt)
+function _solve_trajectory(vfcn,u0,Trange,Δt,alg; kwargs...)
   Path = ODEProblem(vfcn,u0,Trange)
-  sol = solve(Path,Tsit5(), dt = Δt, maxiters = 1e8, adaptive = false, dense = false)
+  sol = OrdinaryDiffEq.solve(Path,alg; dt = Δt, maxiters = 1e8, adaptive = false, dense = false, kwargs...)
 end
 
-function _solve_streamline(vfcn,u0,Trange,Δt,p)
+function _solve_streamline(vfcn,u0,Trange,Δt,p,alg; kwargs...)
   Path = ODEProblem(vfcn,u0,Trange,p)
-  sol = solve(Path,Tsit5(), dt = Δt, maxiters = 1e8, adaptive = false, dense = false)
+  sol = OrdinaryDiffEq.solve(Path,alg; dt = Δt, maxiters = 1e8, adaptive = false, dense = false, kwargs...)
 end
 
 
@@ -175,6 +192,14 @@ function _vfcn_nonautonomous_frozentime(R,p,t,u,v)
   dR[2] = v(R[1],R[2],p)
 
  return dR
+end
+
+function _vfcn_interpolated_series!(dR,R,p,t,vr,tr)
+  jr = searchsortedfirst(tr,t)
+  u, v = vr[jr]
+  dR.x[1] .= u.(R.x[1],R.x[2])
+  dR.x[2] .= v.(R.x[1],R.x[2])
+  return dR
 end
 
 function _field_along_trajectory(v::VectorGridData,sys::ILMSystem,traj::ODESolution,::Val{0})
@@ -209,3 +234,14 @@ end
 _field_along_trajectory(s::ScalarGridData,sys::ILMSystem,traj::ODESolution,::Val{1}) =
        ddt(_field_along_trajectory(s,sys,traj,Val(0)),traj.t)
    
+
+function _check_times(tr,Trange,Δt)
+    ti, tf = Trange
+    ji, jf = searchsortedfirst(tr,ti), searchsortedfirst(tr,tf)
+    @assert tr[ji] ≈ ti "First entry in time range is not in supplied time array"
+    @assert tr[jf] ≈ tf "Last entry in time range is not in supplied time array"
+    @assert abs(Δt/step(tr)) >= 1 "Supplied time step size must be >= than step size in supplied time array"
+    @assert mod(Δt,step(tr)) ≈ 0 "Supplied time step size must be integer multiple of step size in supplied time array"
+    return ti, tf
+end
+       
